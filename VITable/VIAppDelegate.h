d0a10c9fb4b0e4582f4a957bebfbc4645b60add7
Vikas Sharma <vikas.kumar5885@gmail.com>
2014-04-05 20:04:22 +0530

    First commit of the most most AWSOME TableView project !

---
+++ VITable/VIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VIAppDelegate.h
+//  VITable
+//
+//  Created by Vikas Sharma on 05/04/14.
+//  Copyright (c) 2014 Vikas Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VITable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VITable
+//
+//  Created by Vikas Sharma on 05/04/14.
+//  Copyright (c) 2014 Vikas Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VIAppDelegate class]));
+    }
+}
